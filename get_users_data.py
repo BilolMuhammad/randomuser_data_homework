ac1a76716a57d16f3750084330c7dde0b72e54e7
Bilol <mamatqulov@gmail.com>
2023-01-20 09:20:41 +0500

    get_users_data

--- get_users_data.py
+++ get_users_data.py
@@ -1,9 +1,10 @@
 import get_data
 
-def get_users_data(data:dict) -> list:
+
+def get_users_data(data: dict) -> list:
     """
     Take the data of the first name, last name and phone number. Return the list.
-    
+
     The list items are as follows:
         {"first_name": "Dominic", "last_name":"Warholm", "phone_number": "27707465"}
 
@@ -12,3 +13,13 @@ def get_users_data(data:dict) -> list:
     Returns:
         list: users data list
     """
+    data_user = []
+    for n in range(len(data['results'])):
+        data_user.append(data['results'][n]['name']['first'])
+        data_user.append(data['results'][n]['name']['last'])
+        data_user.append(data['results'][n]['phone'])
+    return data_user
+
+
+data = get_data.get_data('randomuser_data.json')
+print(get_users_data(data))
